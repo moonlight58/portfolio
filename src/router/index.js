e190461590f4da252fc7ff7d43b15d34e173474d
moonlight58 <gaelrothlin@gmail.com>
2024-12-04 23:10:36 +0100

    added path to the 'important' project

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import SpotifyTuiView from "@/views/projets/Spotify-TuiView.vue";
+import SteamToLinuxView from "@/views/projets/SteamToLinuxView.vue";
+import BienView from "@/views/projets/BienView.vue";
+import RoiDesRosesView from "@/views/projets/RoiDesRosesView.vue";
 
 Vue.use(VueRouter)
 
@@ -16,16 +20,24 @@ const routes = [
     component: Home
   },
   {
-    path: '/personal/spotify-tui'
+    path: '/personal/spotify-tui',
+    name: 'spotify-tui',
+    component: SpotifyTuiView
   },
   {
-    path: '/personal/steamtolinux'
+    path: '/personal/steamtolinux',
+    name: 'steamtolinux',
+    component: SteamToLinuxView
   },
   {
-    path: '/uni/bien'
+    path: '/uni/bien',
+    name: 'bien',
+    component: BienView
   },
   {
     path: '/uni/rdr',
+    name: 'rdr',
+    component: RoiDesRosesView
   },
   {
     path: '/about',
